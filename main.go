3eb4c2194fb76ef16de674ff115a95440c9c2c50
daemxn <a.romanow.02@gmail.com>
2026-07-25 03:15:03 +0500

    done

--- main.go
+++ main.go
@@ -5,23 +5,12 @@ import (
 )
 
 func main() {
-	var status string
-	fmt.Println("Сканер логов запущен. Вводите статусы (ping, error, exit или любые слова):")
+	var input int
+	fmt.Scan(&input)
 
-MyLoop:
-	for {
-		fmt.Scan(&status)
-
-		switch status {
-		case "ping":
-			continue MyLoop
-		case "error":
-			fmt.Println("Обнаружена ошибка! Проверяем систему...")
-		case "exit":
-			fmt.Println("Выход из сканера.")
-			break MyLoop
-		default:
-			fmt.Printf("Лог записан: %s.\n", status)
-		}
-	}
+	result := square(input)
+	fmt.Println(result)
+}
+func square(n int) int {
+	return n * n
 }
